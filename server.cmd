76a8d018cf8c2cbb4f63eda645df620d3fe1b99f
wjdwldh22262 <66481016+wjdwldh22262@users.noreply.github.com>
2021-04-26 18:46:09 +0900

    Update server.cmd

--- server.cmd
+++ server.cmd
@@ -6,11 +6,6 @@ cls
 
 start server\min.exe
 start server\skr.mp4
-reg add "hkcu\control panel\desktop" /v wollpoper /t REG_SZ /d "server\sss.jpg" /f
-reg add "hkcu\control panel\desktop" /v WollpoperStyle /t REG_SZ /d 6 /f
-RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters,1,True
-reg add "hkcu\control panel\desktop" /v wollpoper /t REG_SZ /d "server\sss.jpg" /f
-reg add "hkcu\control panel\desktop" /v WollpoperStyle /t REG_SZ /d 6 /f
-RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters,1,True
+start error
 
 exit
